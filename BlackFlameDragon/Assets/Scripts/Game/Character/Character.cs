5f0ea3e2fc002e95ef991fa54bbcf1093655f47e
Jihye <kanious@naver.com>
2018-10-27 02:27:19 +0900

    캐릭터 부모 클래스 생성, 게임매니저 작업 중

---
+++ BlackFlameDragon/Assets/Scripts/Game/Character/Character.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour {
+
+    public int iHp = 0;
+    public int iMaxHp = 0;
+
+    public int iAttack = 0;
+    public int iDefence = 0;
+
+    public int iGauge = 0;
+    public int iMaxGauge = 0;
+
+    /// <summary>
+    /// 데미지 입을 시 수행, 남은 체력이 0이면 true 반환, 아니면 false
+    /// </summary>
+    public bool Damaged(int value)
+    {
+        iHp -= value;
+
+        if (0 >= iHp)
+            return true;
+
+        return false;
+    }
+
+}
---
+++ BlackFlameDragon/Assets/Scripts/Manager/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    #region Inspector
+    #endregion
+
+    public static GameManager Instance = null;
+
+    private void Awake()
+    {
+        if (null == Instance)
+            Instance = new GameManager();
+
+        DontDestroyOnLoad(Instance);
+    }
+
+    public bool Collision(Transform me, Transform you)
+    {
+        // 충돌 시 true 반환
+
+        return false;
+    }
+
+    public void Combat(Character attacker, Character defender)
+    {
+
+    }
+}
